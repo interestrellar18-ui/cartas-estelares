0a6db3e16afdd50fb59c799d3c205566f054a92f
emillyestrella <inter.estrellar18@gmail.com>
2026-06-04 14:42:41 -0300

    trevas

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+
+  // DESLIGA PWA NO DESENVOLVIMENTO
   disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
+
   reactStrictMode: true,
+
+  // IMPEDE O TURBOPACK DE SURTAR
+  turbopack: {},
+
 });
